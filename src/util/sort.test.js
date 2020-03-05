75cbd921859d8b02572acea64ad36b8827d653c3
Tom Erik Støwer <testower@gmail.com>
2020-03-05 13:01:53 +0100

    Add test for sorting

---
+++ src/util/sort.test.js
@@ -0,0 +1,17 @@
+import { sortBySituationNumber } from './sort';
+
+test('sortBySituationNumber', () => {
+  expect(
+    [
+      { data: { SituationNumber: 'COD:SituationNumber:9' } },
+      { data: { SituationNumber: 'COD:SituationNumber:1' } },
+      { data: { SituationNumber: 'COD:SituationNumber:5' } },
+      { data: { SituationNumber: 'COD:SituationNumber:3' } }
+    ].sort(sortBySituationNumber)
+  ).toEqual([
+    { data: { SituationNumber: 'COD:SituationNumber:1' } },
+    { data: { SituationNumber: 'COD:SituationNumber:3' } },
+    { data: { SituationNumber: 'COD:SituationNumber:5' } },
+    { data: { SituationNumber: 'COD:SituationNumber:9' } }
+  ]);
+});
