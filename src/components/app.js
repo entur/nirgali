af48010736197b7827068f524657931754d4e35b
Tom Erik Støwer <testower@gmail.com>
2020-03-27 16:17:55 +0100

    Lo0cal development with firebase emulators

---
+++ src/components/app.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Overview from './overview';
+import Register from './register';
+import Edit from './edit';
+import api from '../api/api';
+import NavBar from './navbar';
+import Background from '../img/background.jpg';
+
+export default class App extends React.Component {
+  state = {
+    organizations: [],
+    organizationsName: [],
+    selectedOrganization: '',
+    messages: [],
+    lines: []
+  };
+
+  componentDidMount() {
+    this.setOrganizations();
+    this.db = this.props.firebase.firestore();
+  }
+
+  setOrganizations = async () => {
+    if (!this.props.userInfo.roles) {
+      window.alert('You are not assigned to a company');
+      window.location.href = this.props.userInfo.logoutUrl;
+    }
+
+    const allowedCodespaces = this.props.userInfo.roles
+      .map(JSON.parse)
+      .filter(({ r: role }) => role === 'editSX')
+      .map(({ o: org }) => org);
+
+    const response = await api.getAuthorities();
+    const organizations = response.data.authorities.filter(authority =>
+      allowedCodespaces.includes(authority.id.split(':')[0])
+    );
+
+    this.setState(
+      {
+        organizations: organizations.map(({ id }) => id),
+        organizationsName: organizations.map(({ name }) => name),
+        selectedOrganization: organizations[0].id
+      },
+      () => this.getMessagesAndLines()
+    );
+  };
+
+  updateOrganization = selectedOrg => {
+    this.setState(
+      {
+        selectedOrganization: selectedOrg
+      },
+      () => this.getMessagesAndLines()
+    );
+  };
+
+  getMessagesAndLines = async () => {
+    await this.getMessages();
+    await this.getLines();
+  };
+
+  getMessages = async () => {
+    if (this.unsubscribeSnapshotListener) {
+      this.unsubscribeSnapshotListener();
+    }
+    const codespace = this.state.selectedOrganization.split(':')[0];
+    const authority = this.state.selectedOrganization;
+    this.unsubscribeSnapshotListener = this.db
+      .collection(`codespaces/${codespace}/authorities/${authority}/messages`)
+      .onSnapshot(this.onMessagesUpdate);
+  };
+
+  onMessagesUpdate = querySnapshot => {
+    this.setState({
+      messages:
+        querySnapshot.size > 0
+          ? querySnapshot.docs.map(doc => ({
+              id: doc.id,
+              data: doc.data()
+            }))
+          : []
+    });
+  };
+
+  getLines = async () => {
+    const response = await api.getLines(this.state.selectedOrganization);
+    if (response.data) {
+      this.setState({
+        lines: response.data.lines
+      });
+    } else {
+      console.log('Could not find any lines for this organization');
+    }
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <img className="background-image" src={Background} alt="" />
+
+          <Route
+            path="/"
+            render={props => (
+              <NavBar
+                {...props}
+                NavBar
+                onSelectOrganization={this.updateOrganization}
+                user={this.state.organizations}
+                name={this.state.organizationsName}
+                logout={this.props.userInfo.logoutUrl}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Overview {...props} messages={this.state.messages} />
+            )}
+          />
+          <Route
+            path="/edit/:id?"
+            render={props => (
+              <Edit
+                {...props}
+                issue={this.state.messages.find(
+                  ({ id }) => id === props.match.params.id
+                )}
+                lines={this.state.lines}
+                firebase={this.db}
+                organization={this.state.selectedOrganization}
+              />
+            )}
+          />
+          <Route
+            path="/register"
+            render={props => (
+              <Register
+                {...props}
+                firebase={this.db}
+                lines={this.state.lines}
+                organization={this.state.selectedOrganization}
+              />
+            )}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,167 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import auth from './authenticate/auth';
+import App from './components/app';
+import * as firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/index.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import Overview from './components/overview';
-import Register from './components/register';
-import Edit from './components/edit';
-import auth from './authenticate/auth';
-import api from './api/api';
-import NavBar from './components/navbar';
-import Background from './img/background.jpg';
 
-const firebase = require('firebase/app');
-require('firebase/firestore');
-require('firebase/auth');
 
-class App extends React.Component {
-  state = {
-    organizations: [],
-    organizationsName: [],
-    selectedOrganization: '',
-    messages: [],
-    lines: []
-  };
-
-  componentDidMount() {
-    this.setOrganizations();
-    this.db = firebase.firestore();
-  }
-
-  setOrganizations = async () => {
-    if (!this.props.userInfo.roles) {
-      window.alert('You are not assigned to a company');
-      window.location.href = this.props.userInfo.logoutUrl;
-    }
-
-    const allowedCodespaces = this.props.userInfo.roles
-      .map(JSON.parse)
-      .filter(({ r: role }) => role === 'editSX')
-      .map(({ o: org }) => org);
-
-    const response = await api.getAuthorities();
-    const organizations = response.data.authorities.filter(authority =>
-      allowedCodespaces.includes(authority.id.split(':')[0])
-    );
-
-    this.setState(
-      {
-        organizations: organizations.map(({ id }) => id),
-        organizationsName: organizations.map(({ name }) => name),
-        selectedOrganization: organizations[0].id
-      },
-      () => this.getMessagesAndLines()
-    );
-  };
-
-  updateOrganization = selectedOrg => {
-    this.setState(
-      {
-        selectedOrganization: selectedOrg
-      },
-      () => this.getMessagesAndLines()
-    );
-  };
-
-  getMessagesAndLines = async () => {
-    await this.getMessages();
-    await this.getLines();
-  };
-
-  getMessages = async () => {
-    if (this.unsubscribeSnapshotListener) {
-      this.unsubscribeSnapshotListener();
-    }
-    const codespace = this.state.selectedOrganization.split(':')[0];
-    const authority = this.state.selectedOrganization;
-    this.unsubscribeSnapshotListener = this.db
-      .collection(`codespaces/${codespace}/authorities/${authority}/messages`)
-      .onSnapshot(this.onMessagesUpdate);
-  };
-
-  onMessagesUpdate = querySnapshot => {
-    this.setState({
-      messages:
-        querySnapshot.size > 0
-          ? querySnapshot.docs.map(doc => ({
-              id: doc.id,
-              data: doc.data()
-            }))
-          : []
-    });
-  };
-
-  getLines = async () => {
-    const response = await api.getLines(this.state.selectedOrganization);
-    if (response.data) {
-      this.setState({
-        lines: response.data.lines
-      });
-    } else {
-      console.log('Could not find any lines for this organization');
-    }
-  };
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <img className="background-image" src={Background} alt="" />
-
-          <Route
-            path="/"
-            render={props => (
-              <NavBar
-                {...props}
-                NavBar
-                onSelectOrganization={this.updateOrganization}
-                user={this.state.organizations}
-                name={this.state.organizationsName}
-                logout={this.props.userInfo.logoutUrl}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/"
-            render={props => (
-              <Overview {...props} messages={this.state.messages} />
-            )}
-          />
-          <Route
-            path="/edit/:id?"
-            render={props => (
-              <Edit
-                {...props}
-                issue={this.state.messages.find(
-                  ({ id }) => id === props.match.params.id
-                )}
-                lines={this.state.lines}
-                firebase={this.db}
-                organization={this.state.selectedOrganization}
-              />
-            )}
-          />
-          <Route
-            path="/register"
-            render={props => (
-              <Register
-                {...props}
-                firebase={this.db}
-                lines={this.state.lines}
-                organization={this.state.selectedOrganization}
-              />
-            )}
-          />
-        </div>
-      </Router>
-    );
-  }
-}
-
-const renderIndex = userInfo => {
-  ReactDOM.render(<App userInfo={userInfo} />, document.getElementById('root'));
+const renderApp = userInfo => {
+  ReactDOM.render(<App firebase={firebase} userInfo={userInfo} />, document.getElementById('root'));
 };
 
 const init = async () => {
@@ -173,6 +24,7 @@ const init = async () => {
       Authorization: 'Bearer ' + token
     }
   });
+
   const newTokenHandler = async token => {
     const authResponse = await fetch('/api/auth/firebase', headers(token));
     const {firebaseToken} = await authResponse.json();
@@ -181,7 +33,7 @@ const init = async () => {
 
   auth.initAuth(async (token, userInfo) => {
     await newTokenHandler(token);
-    renderIndex(userInfo);
+    renderApp(userInfo);
   }, newTokenHandler);
 }
 
