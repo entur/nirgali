3bec009092d2d48dff567d686e2335f0af68d592
Tom Erik Støwer <testower@gmail.com>
2020-05-27 10:40:19 +0200

    ROR-816 Factor out line and stop pickers and sort options. Put publicCode in front on line options.

---
+++ src/components/line-picker.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Select from 'react-select';
+
+const DEFAULT_PLACEHOLDER = 'Velg linje';
+
+export default ({ lines, onChange }) => {
+  return (
+    <Select
+      placeholder={DEFAULT_PLACEHOLDER}
+      onChange={onChange}
+      options={getLineOptions(lines)}
+    />
+  );
+};
+
+const getLineOptions = lines => {
+  return lines
+    .sort((a, b) => {
+      if (a.publicCode > b.publicCode) return 1;
+      else if (b.publicCode > a.publicCode) return -1;
+      else return 0;
+    })
+    .map(item => ({
+      label: `${item.publicCode} ${item.name} (${item.id})`,
+      value: item.id
+    }));
+};
---
+++ src/components/stop-picker.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Select from 'react-select';
+
+export default ({ stops, isMulti, onChange }) => {
+  return (
+    <Select
+      isMulti={isMulti}
+      placeholder="Velg stopp"
+      onChange={onChange}
+      options={getOptions(stops)}
+    />
+  );
+};
+
+const getOptions = stops => {
+  return stops
+    .sort((a, b) => {
+      if (a.name > b.name) return 1;
+      if (b.name > a.name) return -1;
+      return 0;
+    })
+    .map(item => ({
+      label: item.name + ' - ' + item.stopPlace.id,
+      value: item.stopPlace.id
+    }));
+};
