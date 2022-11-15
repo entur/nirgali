42bcc8bb95695d96780dbc9cff598b65d9cde38b
Tom Erik Støwer <testower@gmail.com>
2022-11-15 12:59:48 +0100

    Add rewrite for auth api

--- functions/auth.js
+++ functions/auth.js
@@ -56,7 +56,6 @@ exports.auth = function(firebaseAdmin) {
 
   app.get('/auth/firebase/auth0', jwtCheck, authenticate);
   app.get('/firebase/auth0', jwtCheck, authenticate);
-  app.get('/api/auth', jwtCheck, authenticate);
 
   return functions
     .region('europe-west1')
