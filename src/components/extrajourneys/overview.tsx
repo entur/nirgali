b784bc2e9508051f8b867ee76d64b78214b8ac4e
Tom Erik Støwer <testower@gmail.com>
2024-02-08 08:44:28 +0100

    Add overview screen

--- src/components/extrajourneys/overview.tsx
+++ src/components/extrajourneys/overview.tsx
@@ -2,9 +2,74 @@ import { Contrast } from '@entur/layout';
 import { SecondaryButton } from '@entur/button';
 import { Switch } from '@entur/form';
 import { useNavigate } from 'react-router-dom';
+import { useSelectedOrganization } from '../../hooks/useSelectedOrganization';
+import { useEffect, useState } from 'react';
+import firebase from 'firebase/compat/app';
+import { DataCell, HeaderCell, Table, TableBody, TableHead, TableRow } from '@entur/table';
+import { getLocalTimeZone, now } from '@internationalized/date';
+
+import { ExtraJourney } from './types';
+// @ts-ignore
+import green from '../../img/green.png';
+// @ts-ignore
+import red from '../../img/red.png';
+
+
+const returnRedOrGreenIcon = (param: ExtraJourney) => {
+  if (
+    param.EstimatedVehicleJourney.ExpiresAtEpochMs >
+    now(getLocalTimeZone()).add({ minutes: 10 }).toDate().getTime()
+  ) {
+    return <img src={green} id="active" alt="" height="30" width="30" />;
+  } else {
+    return <img src={red} id="not_active" alt="" height="30" width="30" />;
+  }
+};
 
 export const Overview = () => {
   const navigate = useNavigate();
+  const selectedOrganization = useSelectedOrganization();
+  const [extrajourneys, setExtraJourneys] = useState([]);
+  const [showCompletedTrips, setShowCompletedTrips] = useState(false);
+
+  const db = firebase.firestore();
+
+  useEffect(() => {
+    const codespace = selectedOrganization.split(':')[0];
+    const authority = selectedOrganization;
+
+    if (!codespace || !authority) {
+      return;
+    }
+
+    let query: any = db
+      .collection(
+        `codespaces/${codespace}/authorities/${authority}/extrajourneys`,
+      );
+
+    if (!showCompletedTrips) {
+      query = query.where('EstimatedVehicleJourney.ExpiresAtEpochMs', '>', new Date().getTime())
+    }
+
+    const unsubscribeSnapshotListener = query
+      .onSnapshot((querySnapshot: any) =>
+        setExtraJourneys(
+          querySnapshot.size > 0
+            ? querySnapshot.docs.map((doc: any) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+            : [],
+        ),
+      );
+
+    return () => {
+      if (unsubscribeSnapshotListener) {
+        unsubscribeSnapshotListener();
+      }
+    };
+  }, [selectedOrganization, db, showCompletedTrips]);
+
   return (
     <>
       <h2 className="text-center text-white">Oversikt</h2>;<br></br>
@@ -21,12 +86,52 @@ export const Overview = () => {
       <br></br>
       <Contrast>
         <div style={{ padding: '0 .5em' }}>
-          <Switch checked={false} onChange={() => {}}>
+          <Switch checked={showCompletedTrips} onChange={(e) => setShowCompletedTrips(e.target.checked)}>
             Vis passerte ekstraavganger
           </Switch>
         </div>
       </Contrast>
       <br></br>
+      <Contrast>
+        <Table>
+          <TableHead>
+            <HeaderCell>Status</HeaderCell>
+            <HeaderCell>Linje</HeaderCell>
+            <HeaderCell>Tur</HeaderCell>
+            <HeaderCell>Fra stasjon</HeaderCell>
+            <HeaderCell>Planlagt avgang</HeaderCell>
+            <HeaderCell>Til stasjon</HeaderCell>
+            <HeaderCell>Planlagt ankomst</HeaderCell>
+          </TableHead>
+          <TableBody>
+            {extrajourneys.map((extrajourney: { data: ExtraJourney }) => (
+              <TableRow>
+                <DataCell>
+                  {returnRedOrGreenIcon(extrajourney.data)}
+                </DataCell>
+                <DataCell>
+                  {extrajourney.data.EstimatedVehicleJourney.PublishedLineName}
+                </DataCell>
+                <DataCell>
+                  {extrajourney.data.EstimatedVehicleJourney.EstimatedVehicleJourneyCode}
+                </DataCell>
+                <DataCell>
+                  {extrajourney.data.EstimatedVehicleJourney.EstimatedCalls.EstimatedCall[0].StopPointRef}
+                </DataCell>
+                <DataCell>
+                  {extrajourney.data.EstimatedVehicleJourney.EstimatedCalls.EstimatedCall[0].AimedDepartureTime}
+                </DataCell>
+                <DataCell>
+                  {extrajourney.data.EstimatedVehicleJourney.EstimatedCalls.EstimatedCall[extrajourney.data.EstimatedVehicleJourney.EstimatedCalls.EstimatedCall.length - 1].StopPointRef}
+                </DataCell>
+                <DataCell>
+                  {extrajourney.data.EstimatedVehicleJourney.EstimatedCalls.EstimatedCall[extrajourney.data.EstimatedVehicleJourney.EstimatedCalls.EstimatedCall.length - 1].AimedArrivalTime}
+                </DataCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Contrast>
     </>
   );
 };
