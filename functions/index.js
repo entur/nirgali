b4246681fa4259fac95a2082b97cb6427a2aef02
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-09-10 20:22:33 +0200

    Remove functions now handled by enlil

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,9 @@
 const { initializeApp } = require('firebase-admin/app');
 
 const auth = require('./auth');
-const xml = require('./xml');
 const logDbWrites = require('./logDbWrites');
-const closeOpenExpiredMessages = require('./closeOpenExpiredMessages');
 
 initializeApp();
 
 exports.auth = auth.auth();
-exports.xml = xml.xml();
-exports.closeOpenExpiredMessages =
-  closeOpenExpiredMessages.closeOpenExpiredMessages();
 exports.logDbWrites = logDbWrites.logDbWrites();
