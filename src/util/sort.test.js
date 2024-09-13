9e72ec66f5c403efd82b8dfadcb260ae0036921e
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-09-13 22:26:48 +0200

    Fix test

--- src/util/sort.test.js
+++ src/util/sort.test.js
@@ -3,15 +3,15 @@ import { sortBySituationNumber } from './sort';
 test('sortBySituationNumber', () => {
   expect(
     [
-      { data: { SituationNumber: 'COD:SituationNumber:9' } },
-      { data: { SituationNumber: 'COD:SituationNumber:1' } },
-      { data: { SituationNumber: 'COD:SituationNumber:5' } },
-      { data: { SituationNumber: 'COD:SituationNumber:3' } },
+      { situationNumber: 'COD:SituationNumber:9' },
+      { situationNumber: 'COD:SituationNumber:1' },
+      { situationNumber: 'COD:SituationNumber:5' },
+      { situationNumber: 'COD:SituationNumber:3' },
     ].sort(sortBySituationNumber),
   ).toEqual([
-    { data: { SituationNumber: 'COD:SituationNumber:1' } },
-    { data: { SituationNumber: 'COD:SituationNumber:3' } },
-    { data: { SituationNumber: 'COD:SituationNumber:5' } },
-    { data: { SituationNumber: 'COD:SituationNumber:9' } },
+    { situationNumber: 'COD:SituationNumber:1' },
+    { situationNumber: 'COD:SituationNumber:3' },
+    { situationNumber: 'COD:SituationNumber:5' },
+    { situationNumber: 'COD:SituationNumber:9' },
   ]);
 });
