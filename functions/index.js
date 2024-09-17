675e8a553d002995f92ab0b2fe808dd553f6ff66
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-09-17 13:45:07 +0200

    Remove functions

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const { initializeApp } = require('firebase-admin/app');
-
-const auth = require('./auth');
-const logDbWrites = require('./logDbWrites');
-
-initializeApp();
-
-exports.auth = auth.auth();
-exports.logDbWrites = logDbWrites.logDbWrites();
